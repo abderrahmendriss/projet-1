717b95b0a3bd4597f145aeb98f469bfa4e5e1c91
amine9898 <47857678+amine9898@users.noreply.github.com>
2019-04-11 14:34:11 +0200

    Create fonction.h

---
+++ fonction.h
@@ -0,0 +1,11 @@
+#ifndef QUIZ_H_
+#define QUIZ_H_
+
+
+void solution(SDL_Surface *ecran,int d);
+void enigma(SDL_Surface *ecran,int d);
+void correct(SDL_Surface *ecran);
+void reponse(SDL_Surface *ecran,int d);
+
+
+#endif
